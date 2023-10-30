b5008347c000e5cd0a5e93673c5212fdc016d7e9
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-10-31 01:57:28 +0300

    add LoginOrRegisterPage to toggle between two pages

---
+++ lib/pages/login_or_register_page.dart
@@ -0,0 +1,33 @@
+import 'package:auth/pages/login_page.dart';
+import 'package:auth/pages/register_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginOrRegisterPage extends StatefulWidget {
+  const LoginOrRegisterPage({super.key});
+
+  @override
+  State<LoginOrRegisterPage> createState() => _LoginOrRegisterPageState();
+}
+
+class _LoginOrRegisterPageState extends State<LoginOrRegisterPage> {
+  // intially show login page
+  bool showLoginPage = true;
+
+  // toggle between login and register page
+  void togglePage() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(registerUserPage: togglePage);
+    } else {
+      return RegisterPage(
+        signInUserPage: togglePage,
+      );
+    }
+  }
+}
