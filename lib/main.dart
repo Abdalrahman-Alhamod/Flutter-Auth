52ac1db9319629ee340f6395902b34ae65b1d966
Abdalrahman-Alhamod <abd.alrahman.alhamod@gmail.com>
2023-10-31 00:25:15 +0300

    Setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:auth/pages/login_page.dart';
 import 'package:auth/pages/register_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Auth());
 }
 
